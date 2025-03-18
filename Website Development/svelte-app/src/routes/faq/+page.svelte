cd1cf89d9265778fe3d6b13503b989dd586be99f
rebecca_hollidge <c00300511@setu.ie>
2025-03-18 14:28:53 +0000

    static pages template

--- Website Development/svelte-app/src/routes/faq/+page.svelte
+++ Website Development/svelte-app/src/routes/faq/+page.svelte
@@ -5,3 +5,12 @@
         console.log("Contact Page Loaded");
     });
 </script>
+
+<p>Questionss  </p>
+<style>
+    p{text-align: center; font-size: 20px;}
+    p{vertical-align: middle;}
+    p{line-height: 30px; }
+    p{ padding-left: 300px; padding-right: 300px;}
+    
+</style>
