87c8b71399f583e9d8128ff3ba67dd188db66be2
(Student  C00299461) Sophia Stanley <C00299461@setu.ie>
2025-04-07 10:47:09 +0100

    Adding login and changed icon to logo

--- Website Development/svelte-app/src/routes/login/+page.svelte
+++ Website Development/svelte-app/src/routes/login/+page.svelte
@@ -1,42 +1,38 @@
+<!-- Source: https://alphonso-javier.medium.com/svelte-simple-login-with-local-storage-and-stores-723759da36b3 -->
+<!-- Modified slightly from that website-->
 <script>
-    import { onMount } from 'svelte';
-    let email = "";
-    let isValid = false;
-
-    onMount(() => {
-        console.log("Contact Page Loaded");
-        const saved = localStorage.getItem("email");
-        if (saved) email = saved;
-        console.log(email); // this doesn't work right now oopsies
-    });
-
-    function validateEmail() {
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        isValid = regex.test(email);
-    }
-
-    function handleInput(event) {
-        if (isValid)
-        {
-            email = event.target.value;
-            localStorage.setItem("email", email);
-            console.log("good job");
-        }
-        else {
-            console.log("NO");
-        }
-    }
+	import { user } from '$lib/store'
+	import { onDestroy } from 'svelte'
+	let user_value = {"username": "", "password": ""}; // object for storing username and password
+	user.subscribe((u) => (user_value = u)) // changing user value to whatever u is, if u is updated
+	let unsubscribe = user.subscribe((u) => (user_value = u))
+	let username = ""; // username value for input form
+	let password = ""; // password value for input form
+	function handleLogin() {
+		if (!username || !password) {
+			alert('please enter your credentials')
+			return
+		}
+		
+		user.update((u) => u = JSON.stringify({ username: username, password: password }))
+	}
+	function handleLogout() {
+		user.update((u) => (u = '')) // emptying user value after log out
+        console.log("Logged Out Successfully");
+	}
+	$: console.log(user_value)
+	onDestroy(unsubscribe)
 </script>
 
+<svelte:head>
+	<title>Log In</title>
+</svelte:head>
 
-<h1>Log In</h1>
-
-<input type="email" bind:value={email} on:input={validateEmail} placeholder="Enter your email">
-
-<button on:click={handleInput}>Submit</button>
-{#if email}
-    {#if isValid == false}
-        <p>Please enter a valid email</p>
-    {/if}
+{#if !user_value}
+	<input type="username" bind:value={username} placeholder="enter username" />
+	<input type="password" bind:value={password} placeholder="enter password" />
+	<button on:click={handleLogin}>Login</button>
+{:else if user_value}
+	<h3>You are logged in as: {(user_value).username}</h3> <!--The username doesn't display when you first log in, but does after refresh?-->
+	<button on:click={handleLogout}>Logout</button>
 {/if}
-
