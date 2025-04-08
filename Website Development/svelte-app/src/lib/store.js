73fd76ed1a7f0dd864898d69c2e82ac3060496c1
(Student  C00299461) Sophia Stanley <C00299461@setu.ie>
2025-04-08 14:42:26 +0100

    Login page working

--- Website Development/svelte-app/src/lib/store.js
+++ Website Development/svelte-app/src/lib/store.js
@@ -3,9 +3,10 @@
 import { writable } from 'svelte/store'
 import { browser } from '$app/environment'
 
-let persistedUser = browser && localStorage.getItem('user')
-export let user = writable(persistedUser ? JSON.parse(persistedUser) : '')
-
-if (browser) {
+//const persistedUser = browser && localStorage.getItem('user')
+//export let user = writable({username: "", password: ""})
+export const username = writable(browser && localStorage.getItem('username') ? localStorage.getItem('username') : "")
+export const password = writable(browser && localStorage.getItem('password') ? localStorage.getItem('password') : "")
+/*if (browser) {
     user.subscribe(u => localStorage.user = u)
-}
+}*/
